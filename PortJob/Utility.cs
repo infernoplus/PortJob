1fc2a2b1e8aa7eaba9260ce0a31dbf57f10f2fe0
nordgaren <nordgarentv@gmail.com>
2022-06-18 15:51:30 -0700

    Code Style

--- PortJob/Utility.cs
+++ PortJob/Utility.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PortJob
-{
-    class Utility
-    {
+namespace PortJob {
+    class Utility {
 
-        private static readonly char[] _dirSep = new char[] { '\\', '/' };
+        private static readonly char[] _dirSep = { '\\', '/' };
 
         /* Take a full file path and returns just a file name without directory or extensions */
-        public static string PathToFileName(string fileName)
-        {
+        public static string PathToFileName(string fileName) {
             if (fileName.EndsWith("\\") || fileName.EndsWith("/"))
                 fileName = fileName.TrimEnd(_dirSep);
 
@@ -26,11 +23,10 @@ namespace PortJob
             return fileName;
         }
 
-        private static readonly char[] CHAR_NUMS = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static readonly char[] CHAR_NUMS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         /* Take a FBX data channel name and return the index of it as an int */
         /* EX: Normals3 returns 3 */
-        public static int GetChannelIndex(string s)
-        {
+        public static int GetChannelIndex(string s) {
             return int.Parse(s.Substring(s.IndexOfAny(CHAR_NUMS)));
         }
     }
