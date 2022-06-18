4e222969e92f90f43f673d54c9f33bed26de6a60
nordgaren <nordgarentv@gmail.com>
2022-06-18 15:57:28 -0700

    Fixed enum

--- PortJob/ESM.cs
+++ PortJob/ESM.cs
@@ -7,101 +7,51 @@ using Newtonsoft.Json.Linq;
 using System.Numerics;
 using SoulsFormats;
 
-namespace PortJob
-{
+namespace PortJob {
     /* Loads and handles the JSON file of the morrowind.esm that tes3conv outputs */
-    public class ESM
-    {
+    public class ESM {
         private JArray json; // Full unfiltered json of the morrowind.json
         private Dictionary<Type, List<JObject>> recordsMap;
 
-        public enum Type
-        {
-            Header,
-            GameSetting,
-            GlobalVariable,
-            Class,
-            Faction,
-            Race,
-            Sound,
-            Skill,
-            MagicEffect,
-            Script,
-            Region,
-            Birthsign,
-            LandscapeTexture,
-            Spell,
-            Static,
-            Door,
-            MiscItem,
-            Weapon,
-            Container,
-            Creature,
-            Bodypart,
-            Light,
-            Enchantment,
-            Npc,
-            Armor,
-            Clothing,
-            RepairTool,
-            Activator,
-            Apparatus,
-            Lockpick,
-            Probe,
-            Ingredient,
-            Book,
-            Alchemy,
-            LevelledItem,
-            LevelledCreature,
-            Cell,
-            Landscape,
-            PathGrid,
-            SoundGen,
-            Dialogue,
-            Info
+        public enum Type {
+            Header, GameSetting, GlobalVariable, Class, Faction, Race, Sound, Skill, MagicEffect, Script, Region, Birthsign, LandscapeTexture, Spell, Static, Door,
+            MiscItem, Weapon, Container, Creature, Bodypart, Light, Enchantment, Npc, Armor, Clothing, RepairTool, Activator, Apparatus, Lockpick, Probe, Ingredient,
+            Book, Alchemy, LevelledItem, LevelledCreature, Cell, Landscape, PathGrid, SoundGen, Dialogue, Info
         }
 
-        public ESM(string path)
-        {
+        public ESM(string path) {
             string data = File.ReadAllText(path);
             json = JArray.Parse(data);
 
-            recordsMap = new();
+            recordsMap = new Dictionary<Type, List<JObject>>();
             foreach (string name in Enum.GetNames(typeof(Type))) {
                 Enum.TryParse(name, out Type type);
-                recordsMap.Add(type, new());
+                recordsMap.Add(type, new List<JObject>());
             }
 
-            for (int i = 0; i < json.Count; i++)
-            {
+            for (int i = 0; i < json.Count; i++) {
                 JObject record = (JObject)json[i];
 
-                foreach (string name in Enum.GetNames(typeof(Type)))
-                {
-                    if (record["type"].ToString() == name)
-                    {
+                foreach (string name in Enum.GetNames(typeof(Type))) {
+                    if (record["type"].ToString() == name) {
                         Enum.TryParse(name, out Type type);
                         recordsMap[type].Add(record);
                     }
                 }
             }
 
-            foreach (string name in Enum.GetNames(typeof(Type)))
-            {
+            foreach (string name in Enum.GetNames(typeof(Type))) {
                 Enum.TryParse(name, out Type type);
                 Console.WriteLine(name + ": " + recordsMap[type].Count);
             }
         }
 
-        public Cell GetCell(int x, int y)
-        {
+        public Cell GetCell(int x, int y) {
             List<JObject> cells = recordsMap[Type.Cell];
-            foreach (JObject cell in cells)
-            {
+            foreach (JObject cell in cells) {
                 int xx = int.Parse(cell["data"]["grid"][0].ToString());
                 int yy = int.Parse(cell["data"]["grid"][1].ToString());
-                if(x == xx && y == yy)
-                {
+                if (x == xx && y == yy) {
                     return new Cell(this, cell);
                 }
             }
@@ -109,24 +59,20 @@ namespace PortJob
         }
 
         /* List of types that we should search for references */
-        public readonly Type[] VALID_CONTENT_TYPES = new Type[] {
+        public readonly Type[] VALID_CONTENT_TYPES = {
             Type.Sound, Type.Skill, Type.Region, Type.Static, Type.Door, Type.MiscItem, Type.Weapon, Type.Container, Type.Creature, Type.Bodypart, Type.Light, Type.Npc,
             Type.Armor, Type.Clothing, Type.RepairTool, Type.Activator, Type.Apparatus, Type.Lockpick, Type.Probe, Type.Ingredient, Type.Book, Type.Alchemy, Type.LevelledItem,
             Type.LevelledCreature, Type.PathGrid, Type.SoundGen
         };
 
         /* References don't contain any explicit 'type' data so... we just gotta go find it lol */
-        public TypedRecord FindRecordByID(string id)
-        {
-            foreach(Type type in VALID_CONTENT_TYPES)
-            {
+        public TypedRecord FindRecordByID(string id) {
+            foreach (Type type in VALID_CONTENT_TYPES) {
                 List<JObject> records = recordsMap[type];
 
-                for(int i=0;i<records.Count;i++)
-                {
+                for (int i = 0; i < records.Count; i++) {
                     JObject record = records[i];
-                    if (record["id"] != null && record["id"].ToString() == id)
-                    {
+                    if (record["id"] != null && record["id"].ToString() == id) {
                         return new TypedRecord(type, record);
                     }
                 }
@@ -135,8 +81,7 @@ namespace PortJob
         }
     }
 
-    public class Cell
-    {
+    public class Cell {
         public static readonly float CELL_SIZE = 81.92f;
 
         public readonly string name;
@@ -150,8 +95,7 @@ namespace PortJob
 
         public readonly List<Content> content;
 
-        public Cell(ESM esm, JObject data)
-        {
+        public Cell(ESM esm, JObject data) {
             name = data["id"].ToString();
             region = data["region"].ToString();
 
@@ -165,24 +109,21 @@ namespace PortJob
 
             JArray flc = (JArray)(data["flags"]);
             flags = new int[flc.Count];
-            for (int i = 0; i < flc.Count; i++)
-            {
+            for (int i = 0; i < flc.Count; i++) {
                 flags[i] = int.Parse(flc[i].ToString());
             }
 
-            content = new();
+            content = new List<Content>();
 
             JArray refc = (JArray)(data["references"]);
-            for (int i=0;i<refc.Count;i++)
-            {
-                JObject reference = (JObject)(refc[i]);
+            for (int i = 0; i < refc.Count; i++) {
+                JObject reference = (JObject)();
                 content.Add(new Content(esm, reference));
             }
         }
     }
 
-    public class Content
-    {
+    public class Content {
         public readonly string id;
         public readonly ESM.Type type;
 
@@ -190,8 +131,7 @@ namespace PortJob
 
         public readonly Vector3 position, rotation;
 
-        public Content(ESM esm, JObject data)
-        {
+        public Content(ESM esm, JObject data) {
             id = data["id"].ToString();
 
             TypedRecord tr = esm.FindRecordByID(id);
@@ -208,26 +148,22 @@ namespace PortJob
             float j = float.Parse(((JArray)(data["rotation"]))[2].ToString()) - (float)Math.PI;
 
             position = new Vector3(x, y, z) * FBXConverter.GLOBAL_SCALE;
-            rotation = new Vector3(i, j, k) * (float)(180/Math.PI);
+            rotation = new Vector3(i, j, k) * (float)(180 / Math.PI);
         }
     }
 
-    public class Grid
-    {
+    public class Grid {
         public readonly int x, y;
-        public Grid(int xx, int yy)
-        {
+        public Grid(int xx, int yy) {
             x = xx; y = yy;
         }
     }
 
-    public class TypedRecord
-    {
+    public class TypedRecord {
         public ESM.Type type;
         public JObject record;
 
-        public TypedRecord(ESM.Type t, JObject r)
-        {
+        public TypedRecord(ESM.Type t, JObject r) {
             type = t;
             record = r;
         }
