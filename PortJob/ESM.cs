1a63f45fe252c33c73d3633abae95269d62a444f
InfernoPlus <dmtinfernocid@gmail.com>
2022-06-17 19:31:30 -0500

    Files.

---
+++ PortJob/ESM.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+using System.Numerics;
+using SoulsFormats;
+
+namespace PortJob
+{
+    /* Loads and handles the JSON file of the morrowind.esm that tes3conv outputs */
+    public class ESM
+    {
+        private JArray json; // Full unfiltered json of the morrowind.json
+        private Dictionary<Type, List<JObject>> recordsMap;
+
+        public enum Type
+        {
+            Header,
+            GameSetting,
+            GlobalVariable,
+            Class,
+            Faction,
+            Race,
+            Sound,
+            Skill,
+            MagicEffect,
+            Script,
+            Region,
+            Birthsign,
+            LandscapeTexture,
+            Spell,
+            Static,
+            Door,
+            MiscItem,
+            Weapon,
+            Container,
+            Creature,
+            Bodypart,
+            Light,
+            Enchantment,
+            Npc,
+            Armor,
+            Clothing,
+            RepairTool,
+            Activator,
+            Apparatus,
+            Lockpick,
+            Probe,
+            Ingredient,
+            Book,
+            Alchemy,
+            LevelledItem,
+            LevelledCreature,
+            Cell,
+            Landscape,
+            PathGrid,
+            SoundGen,
+            Dialogue,
+            Info
+        }
+
+        public ESM(string path)
+        {
+            string data = File.ReadAllText(path);
+            json = JArray.Parse(data);
+
+            recordsMap = new();
+            foreach (string name in Enum.GetNames(typeof(Type))) {
+                Enum.TryParse(name, out Type type);
+                recordsMap.Add(type, new());
+            }
+
+            for (int i = 0; i < json.Count; i++)
+            {
+                JObject record = (JObject)json[i];
+
+                foreach (string name in Enum.GetNames(typeof(Type)))
+                {
+                    if (record["type"].ToString() == name)
+                    {
+                        Enum.TryParse(name, out Type type);
+                        recordsMap[type].Add(record);
+                    }
+                }
+            }
+
+            foreach (string name in Enum.GetNames(typeof(Type)))
+            {
+                Enum.TryParse(name, out Type type);
+                Console.WriteLine(name + ": " + recordsMap[type].Count);
+            }
+        }
+
+        public Cell GetCell(int x, int y)
+        {
+            List<JObject> cells = recordsMap[Type.Cell];
+            foreach (JObject cell in cells)
+            {
+                int xx = int.Parse(cell["data"]["grid"][0].ToString());
+                int yy = int.Parse(cell["data"]["grid"][1].ToString());
+                if(x == xx && y == yy)
+                {
+                    return new Cell(this, cell);
+                }
+            }
+            return null; // Out of bounds
+        }
+
+        /* List of types that we should search for references */
+        public readonly Type[] VALID_CONTENT_TYPES = new Type[] {
+            Type.Sound, Type.Skill, Type.Region, Type.Static, Type.Door, Type.MiscItem, Type.Weapon, Type.Container, Type.Creature, Type.Bodypart, Type.Light, Type.Npc,
+            Type.Armor, Type.Clothing, Type.RepairTool, Type.Activator, Type.Apparatus, Type.Lockpick, Type.Probe, Type.Ingredient, Type.Book, Type.Alchemy, Type.LevelledItem,
+            Type.LevelledCreature, Type.PathGrid, Type.SoundGen
+        };
+
+        /* References don't contain any explicit 'type' data so... we just gotta go find it lol */
+        public TypedRecord FindRecordByID(string id)
+        {
+            foreach(Type type in VALID_CONTENT_TYPES)
+            {
+                List<JObject> records = recordsMap[type];
+
+                for(int i=0;i<records.Count;i++)
+                {
+                    JObject record = records[i];
+                    if (record["id"] != null && record["id"].ToString() == id)
+                    {
+                        return new TypedRecord(type, record);
+                    }
+                }
+            }
+            return null; // Not found!
+        }
+    }
+
+    public class Cell
+    {
+        public readonly string name;
+        public readonly string region;
+        public readonly Grid grid;
+
+        public readonly int flag;
+        public readonly int[] flags;
+
+        public readonly List<Content> content;
+
+        public Cell(ESM esm, JObject data)
+        {
+            name = data["id"].ToString();
+            region = data["region"].ToString();
+
+            int x = int.Parse(data["data"]["grid"][0].ToString());
+            int y = int.Parse(data["data"]["grid"][1].ToString());
+            grid = new Grid(x, y);
+
+            flag = int.Parse(data["data"]["flags"].ToString());
+
+            JArray flc = (JArray)(data["flags"]);
+            flags = new int[flc.Count];
+            for (int i = 0; i < flc.Count; i++)
+            {
+                flags[i] = int.Parse(flc[i].ToString());
+            }
+
+            content = new();
+
+            JArray refc = (JArray)(data["references"]);
+            for (int i=0;i<refc.Count;i++)
+            {
+                JObject reference = (JObject)(refc[i]);
+                content.Add(new Content(esm, reference));
+            }
+        }
+    }
+
+    public class Content
+    {
+        public readonly string id;
+        public readonly ESM.Type type;
+
+        public readonly string mesh;
+
+        public readonly Vector3 position, rotation;
+
+        public Content(ESM esm, JObject data)
+        {
+            id = data["id"].ToString();
+
+            TypedRecord tr = esm.FindRecordByID(id);
+            type = tr.type;
+
+            if (tr.record["mesh"] != null) { mesh = tr.record["mesh"].ToString(); }
+
+            float x = float.Parse(((JArray)(data["translation"]))[0].ToString());
+            float y = float.Parse(((JArray)(data["translation"]))[1].ToString());
+            float z = float.Parse(((JArray)(data["translation"]))[2].ToString());
+
+            float i = float.Parse(((JArray)(data["rotation"]))[0].ToString());
+            float j = float.Parse(((JArray)(data["rotation"]))[1].ToString());
+            float k = float.Parse(((JArray)(data["rotation"]))[2].ToString());
+
+            position = new Vector3(x, y, z);
+            rotation = new Vector3(x, y, z);
+        }
+    }
+
+    public class Grid
+    {
+        public readonly int x, y;
+        public Grid(int xx, int yy)
+        {
+            x = xx; y = yy;
+        }
+    }
+
+    public class TypedRecord
+    {
+        public ESM.Type type;
+        public JObject record;
+
+        public TypedRecord(ESM.Type t, JObject r)
+        {
+            type = t;
+            record = r;
+        }
+    }
+}
